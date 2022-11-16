fd428346baf200799153ac27e7d034b5c670876d
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2022-11-16 22:32:36 +0100

    Initial Commit

---
+++ apps/car-net/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ libs/entity-ui/components/README.md
@@ -0,0 +1,7 @@
+# entity-ui-components
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test entity-ui-components` to execute the unit tests.
