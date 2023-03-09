069e441516d0eb4c0012eaaf107f224cfa24daed
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2023-03-09 23:46:12 +0100

    Added User Entity

--- apps/data-api/src/app/user/user.controller.ts
+++ apps/data-api/src/app/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { User } from "./user.schema";
-import { InjectToken, Token } from "../auth/token.decorator";
+//  import { InjectToken, Token } from "../auth/token.decorator";
 import { isEmail } from "class-validator";
 
 @Controller()
@@ -9,22 +9,17 @@ export class UserController {
   constructor(private userService: UserService) {
   }
 
-  @Get()
-  async findAll(@InjectToken() token: Token): Promise<User[]> {
-    return this.userService.findAll(token.id);
-  }
+//   @Get()
+//   async findAll(@InjectToken() token: Token): Promise<User[]> {
+//     return this.userService.findAll(token.id);
+//   }
 
-  @Get(":identifier")
-  async findOne(@Param("identifier") identifier: string): Promise<User> {
-    if (isEmail(identifier)) {
-      return this.userService.findOneByEmail(identifier);
-    }
+//   @Get(":identifier")
+//   async findOne(@Param("identifier") identifier: string): Promise<User> {
+//     if (isEmail(identifier)) {
+//       return this.userService.findOneByEmail(identifier);
+//     }
 
-    return this.userService.findOne(identifier);
-  }
-
-  @Get(":id/follow")
-  async follow(@InjectToken() token: Token, @Param("id") id: string): Promise<void> {
-    return this.userService.follow(token.id, id);
-  }
+//     return this.userService.findOne(identifier);
+//   }
 }
