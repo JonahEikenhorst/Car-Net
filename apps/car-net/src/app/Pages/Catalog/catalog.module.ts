3a4c7ef5fa4a76ba759fb11e0830afbc1fc30405
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2023-03-31 02:18:55 +0200

    add: Catalog & Garages page

---
+++ apps/car-net/src/app/Pages/Catalog/catalog.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    RouterModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
+  exports: [
+  ],
+  providers: [
+  ]
+})
+export class GarageModule {}
--- apps/car-net/src/app/app.routes.ts
+++ apps/car-net/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './Pages/Auth/login/login.component';
 import { RegisterComponent } from './Pages/Auth/register/register.component';
 import { GarageComponent } from './Pages/Garage/Garage-Page/garage.component';
 import { HomeComponent } from './Pages/Home/home.component';
+import { CatalogComponent } from './Pages/Catalog/Catalog-Page/catalog.component';
+import { GaragesComponent } from './Pages/Garages/Garages-Page/garages.component';
  
 export const appRoutes: Route[] = [
     { path: '', pathMatch: 'full', component: HomeComponent },
@@ -12,6 +14,8 @@ export const appRoutes: Route[] = [
     { path: 'login', pathMatch: 'full', component: LoginComponent },
     { path: 'register', pathMatch: 'full', component: RegisterComponent },
     { path: 'garage', pathMatch: 'full', component: GarageComponent },
+    { path: 'catalog', pathMatch: 'full', component: CatalogComponent },
+    { path: 'garages', pathMatch: 'full', component: GaragesComponent },
     { path: 'garage', loadChildren: () => import ('./Pages/Garage/garage.module').then(m => m.GarageModule) },
     { path: 'brands', loadChildren: () => import ('./Components/brand/brand.module').then(m => m.BrandModule) },
 ];
