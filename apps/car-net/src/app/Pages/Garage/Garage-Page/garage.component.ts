6a227cd4d55ef603f6bb93215a666d5197639599
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2023-04-04 02:09:59 +0200

    fix: load personal garage in front-end

--- apps/car-net/src/app/Pages/Garage/Garage-Page/garage.component.ts
+++ apps/car-net/src/app/Pages/Garage/Garage-Page/garage.component.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @angular-eslint/use-lifecycle-interface */
-import { Component, Input } from '@angular/core';
-import { CarInterface, UserInterface } from '@car-net/interfaces';
+import { Component, OnInit } from '@angular/core';
+import { CarInterface, GarageInterface, UserInterface } from '@car-net/interfaces';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../Auth/auth.service';
 import { GarageService } from '../garage.service';
@@ -10,18 +10,23 @@ import { GarageService } from '../garage.service';
   templateUrl: './garage.component.html',
   styleUrls: ['./garage.component.css'],
 })
-export class GarageComponent {
-  cars$: Observable<CarInterface[]> = new Observable<CarInterface[]>(
-    (observer) => {
-      this.garageService.findAll().subscribe((cars) => observer.next(cars));
-    }
-  );
+export class GarageComponent implements OnInit {
+  username: string | undefined;
+  garage$: Observable<GarageInterface>;
+  user$: Observable<UserInterface> | undefined;
+  owner: UserInterface | undefined;
+  email: string | null = localStorage.getItem('email');
+  cars$: Observable<CarInterface[]> | undefined;
 
-  @Input()
-  user$: Observable<UserInterface> = new Observable<UserInterface>(observer => {
-    this.authService.currentUser.subscribe((user) => observer.next(user));
-  });
   
   constructor(private garageService: GarageService, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.user$ = this.garageService.findOneUser(this.email);
+
+    this.cars$ = this.garageService.findMyCars(this.email);
+    
+    this.user$.subscribe((user) => { this.username = user.username;});
+  }
 
 }
