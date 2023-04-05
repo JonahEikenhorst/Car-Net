f74d85a6dba7d0f3f21e9e51dc1eb0f0c36bfe31
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2023-04-06 00:44:38 +0200

    add: additional requests

--- apps/car-net/src/app/Pages/Garages/garages.service.ts
+++ apps/car-net/src/app/Pages/Garages/garages.service.ts
@@ -16,6 +16,10 @@ export class GaragesService {
     return this.http.get<GarageInterface[]>("http://localhost:3333/api/garages");
   }
 
+  findUserByEmail(email: string | null): Observable<UserInterface> {
+    return this.http.get<UserInterface>(`http://localhost:3333/api/users/email/${email}`);
+  }
+  
   findOneUser(id: string | null): Observable<UserInterface> {
     return this.http.get<UserInterface>(`http://localhost:3333/api/users/${id}`);
   }
@@ -23,6 +27,19 @@ export class GaragesService {
   findMyCars(email: string | null): Observable<CarInterface[]> {
     return this.http.get<CarInterface[]>(`http://localhost:3333/api/garages/cars/${email}`);
   }
+
+  likeGarage(garageName: string | null, email: string | null): Observable<GarageInterface> {
+    return this.http.get<GarageInterface>(`http://localhost:3333/api/garages/like/${garageName}/${email}`);
+  }
+
+  findGarageByName(garageName: string): Observable<GarageInterface> {
+    return this.http.get<GarageInterface>(`http://localhost:3333/api/garages/name/${garageName}`);
+  }
   
+  findIdByName(garageName: string): Observable<string> {
+    return this.http.get<string>(`http://localhost:3333/api/garages/garageId/${garageName}`);
+  }
+
+
 
 }
