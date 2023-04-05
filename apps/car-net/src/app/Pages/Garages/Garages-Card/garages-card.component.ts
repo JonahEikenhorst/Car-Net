7a5d6fd925fd72b08850c11832f0237dc35f3f8f
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2023-04-06 00:44:51 +0200

    add: garages page

--- apps/car-net/src/app/Pages/Garages/Garages-Card/garages-card.component.ts
+++ apps/car-net/src/app/Pages/Garages/Garages-Card/garages-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from "@angular/core";
-import { CarInterface, GarageInterface } from "@car-net/interfaces";
+import { CarInterface, GarageInterface, UserInterface } from "@car-net/interfaces";
+import { GaragesService } from "../garages.service";
+import { AuthService } from "../../Auth/auth.service";
+import { Observable, catchError } from "rxjs";
 
 @Component({
   selector: "car-net-garages-card",
@@ -9,11 +12,24 @@ import { CarInterface, GarageInterface } from "@car-net/interfaces";
 export class GaragesCardComponent implements OnInit {
   numberOfLikes: number;
   numberOfCarsInGarage: number;
+
+  
   @Input()
   garage: GarageInterface;
   firstCar: CarInterface;
   secondCar: CarInterface;
   thirdCar: CarInterface;
+
+  user$: Observable<UserInterface> | undefined;
+  like$: Observable<GarageInterface> | undefined;
+
+  liked: string[];
+  error: string;
+
+  alreadyLiked: boolean;
+
+  constructor(private garagesService: GaragesService, private authService: AuthService) {}
+
 
   ngOnInit() {
     this.numberOfLikes = this.garage.likes.length;
@@ -21,5 +37,28 @@ export class GaragesCardComponent implements OnInit {
     this.firstCar = this.garage.cars[0];
     this.secondCar = this.garage.cars[1];
     this.thirdCar = this.garage.cars[2];
+
+    this.liked = [];
+
+    this.user$ = this.garagesService.findUserByEmail(localStorage.getItem("email"));
   }
+
+  likeGarage() {
+    const garageName = this.garage.garageName; 
+    this.user$?.subscribe((user) => { for(const garagee of user.likedGarages) {
+      this.liked.push(garagee.toString());
+    }});
+    
+    if(this.liked.includes(garageName)) {
+      console.log("You already liked this garage!");
+      this.alreadyLiked = true;
+    } else {
+      const email = localStorage.getItem("email");
+      const like$ = this.garagesService.likeGarage(garageName, email).subscribe(()=>{}, (err) => { alert("Error: " + err.error.message + ""); if(!err) {
+        this.numberOfLikes++;
+      }});
+    }
+
+  }
+
 }
