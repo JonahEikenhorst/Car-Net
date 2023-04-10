60e85bc6cfeac776417cfed461463ca1e9335a0f
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2023-04-10 23:37:04 +0200

    fix: Bringing Application to Production

--- apps/car-net/src/app/Pages/Garage/garage.service.ts
+++ apps/car-net/src/app/Pages/Garage/garage.service.ts
@@ -13,19 +13,19 @@ export class GarageService {
   }
 
   findAll(): Observable<CarInterface[]> {
-    return this.http.get<CarInterface[]>("http://localhost:3333/api/cars");
+    return this.http.get<CarInterface[]>("https://car-net.up.railway.app/api/cars");
   }
 
   findOneUser(id: string | null): Observable<UserInterface> {
-    return this.http.get<UserInterface>(`http://localhost:3333/api/users/${id}`);
+    return this.http.get<UserInterface>(`https://car-net.up.railway.app/api/users/${id}`);
   }
 
   findMyCars(email: string | null): Observable<CarInterface[]> {
-    return this.http.get<CarInterface[]>(`http://localhost:3333/api/garages/cars/${email}`);
+    return this.http.get<CarInterface[]>(`https://car-net.up.railway.app/api/garages/cars/${email}`);
   }
 
   createGarage(garageName: string | null, email: string | null): Observable<GarageInterface> {
-    return this.http.post<GarageInterface>("http://localhost:3333/api/garages", { garageName, email });
+    return this.http.post<GarageInterface>("https://car-net.up.railway.app/api/garages", { garageName, email });
   }
   
 
