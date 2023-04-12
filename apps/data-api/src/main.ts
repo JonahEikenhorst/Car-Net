be8bbeb5254bd5c006f0219ff108de6cd732af24
Jonah Eikenhorst <jonah.eikenhorst@gmail.com>
2023-04-13 00:46:01 +0200

    add: Swagger configuration

--- apps/data-api/src/main.ts
+++ apps/data-api/src/main.ts
@@ -1,11 +1,7 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 
@@ -16,6 +12,16 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   app.enableCors({ origin: "*" });
+
+  const config = new DocumentBuilder()
+    .setTitle('Car-Net API')
+    .setDescription('The Car-Net data-Api endpoints')
+    .setVersion('1.0')
+    .addTag('car-net')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
